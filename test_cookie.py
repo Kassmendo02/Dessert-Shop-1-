29cf3c680ac17fdf5f467992bca6b6851138accb
Kassandra Mendoza La Torre <Kassmendoza147@gmail.com>
2025-12-05 19:45:58 -0700

    Update test_cookie.py

--- test_cookie.py
+++ test_cookie.py
@@ -37,3 +37,44 @@ def test_cookie_calculate_tax():
 def test_cookie_packaging():
     ck = Cookie("Chocolate Chip", 6, 3.99)
     assert ck.packaging == "Box"
+
+def test_cookie_can_combine_true():
+    ck1 = Cookie("Chocolate Chip", 6, 3.99)
+    ck2 = Cookie("Chocolate Chip", 12, 3.99)
+    assert ck1.can_combine(ck2) is True
+
+
+def test_cookie_can_combine_false_different_price():
+    ck1 = Cookie("Chocolate Chip", 6, 3.99)
+    ck2 = Cookie("Chocolate Chip", 12, 4.99)
+    assert ck1.can_combine(ck2) is False
+
+
+def test_cookie_can_combine_false_different_name():
+    ck1 = Cookie("Oatmeal", 6, 3.99)
+    ck2 = Cookie("Chocolate Chip", 6, 3.99)
+    assert ck1.can_combine(ck2) is False
+
+
+def test_cookie_can_combine_false_not_cookie():
+    ck1 = Cookie("Chocolate Chip", 6, 3.99)
+    other = "not a cookie"
+    assert ck1.can_combine(other) is False
+
+
+def test_cookie_combine_success():
+    ck1 = Cookie("Chocolate Chip", 6, 3.99)
+    ck2 = Cookie("Chocolate Chip", 12, 3.99)
+    ck1.combine(ck2)
+    assert ck1.cookie_quantity == 18
+    assert ck1.price_per_dozen == 3.99
+
+
+def test_cookie_combine_type_error():
+    ck1 = Cookie("Chocolate Chip", 6, 3.99)
+    other = 123  # not a Cookie
+    try:
+        ck1.combine(other)
+        assert False     # should NOT reach here
+    except TypeError:
+        assert True
